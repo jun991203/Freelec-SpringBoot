7199f894e24fc38ae4cbce9aa91b359c2459521a
jun991203 <jun991203@naver.com>
2021-03-25 12:00:25 +0900

    chap3 : JPA 다뤄보기

---
+++ src/main/java/com/junyoung/springboot/domain/posts/Posts.java
@@ -0,0 +1,41 @@
+package com.junyoung.springboot.domain.posts;
+
+import com.junyoung.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+    // 실제 DB의 테이블과 매칭될 클래스
+    // 보통 Entity 클래스라고도 함
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
+
